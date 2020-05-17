4a560d4599a6c03b63460030391b365742e43d32
anthony <antonio.defalco2@studenti.uniparthenope.it>
2020-05-18 01:15:21 +0200

    commit

--- euclidean_distance_similarity.m
+++ euclidean_distance_similarity.m
@@ -2,11 +2,12 @@ function distmat=euclidean_distance_similarity(x,y,mu,dist_xn,dist_yn)
     distmat = zeros( size(x,1), size(y,1) );
     for i=1:size(x,1)
         for j=1:size(y,1)
-            dist=sqrt((x(i,:)-y(j,:)));
-            dist2=dist.^2;
+            dist=(x(i,:)-y(j,:));
+            dist2 = dist*dist';
+            dist = sqrt(dist2);
             epsilon_ij = (dist_xn(i) + dist_yn(j) + dist)/3;
-            buff=dist2./(mu*epsilon_ij);
-            distmat(i,j)=buff*buff';
+            buff=dist2/(mu*epsilon_ij);
+            distmat(i,j)=buff;
         end
     end
 end
