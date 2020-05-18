a8336f76f220a5315355e4795390a500840e4363
anthony <antonio.defalco2@studenti.uniparthenope.it>
2020-05-18 12:48:45 +0200

    commit

--- scaled_exp_similarity_kernel.m
+++ scaled_exp_similarity_kernel.m
@@ -1,9 +1,7 @@
 function [K,Ks,Kss]=scaled_exp_similarity_kernel(svar,x,y,dist_xn,dist_yn,mu)
 
     K   =  svar*exp(-0.5*euclidean_distance_similarity(x,x,mu,dist_xn,dist_xn));
-    K = (K + K')/2;
     Ks = svar*exp(-0.5*euclidean_distance_similarity(x,y,mu,dist_xn,dist_yn));  
-   
-    
     Kss  = svar*ones(size(y,1),1);
+    
 end
