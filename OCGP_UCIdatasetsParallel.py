5bf3e012bb67ac8f4e827344f687212d9a19d3b4
AntonioDeFalco <antoniodf92na@gmail.com>
2020-10-05 18:13:35 +0200

    commit

--- OCGP_UCIdatasetsParallel.py
+++ OCGP_UCIdatasetsParallel.py
@@ -63,11 +63,15 @@ def getAUC_OCGP_UCI(posSamples, negSamples, kernel,score,id,AUCs):
 
     if kernel == "adaptive":
         p = 2
-        ocgp.adaptiveKernel(X_train,X_test,p)
+        X_train, X_test = ocgp.preprocessing(X_train, X_test, "zscore")
+        ls = ocgp.adaptiveHyper(X_train, p)
+        ocgp.adaptiveKernel(X_train, X_test, p, ls)
     elif kernel == "scaled":
         v = 0.8
         N = 5
-        ocgp.scaledKernel(X_train, X_test, v, N)
+        X_train, X_test = ocgp.preprocessing(X_train, X_test, "zscore")
+        meanDist_xn, meanDist_yn = ocgp.scaledHyper(X_train, X_test, N)
+        ocgp.scaledKernel(X_train, X_test, v, N, meanDist_xn, meanDist_yn)
 
     scores = ocgp.getGPRscore(score)
     fpr, tpr, thresholds = metrics.roc_curve(y_test, scores)
